b6d1761503904c9b79410d70e20dec6eedf1e01d
Adil <yusubluadil00@gmail.com>
2024-10-15 11:31:20 +0400

    added some files

---
+++ functions.py
@@ -0,0 +1,44 @@
+# def func_name(param1, param2, ...):
+#     kod bloku
+#     return netice
+
+
+# def say_hi():
+#     print('salam')
+
+# say_hi()
+
+
+# 1
+# def multiply():
+#     num1 = int(input('1-ci eded: ')) # 5
+#     num2 = int(input('2-ci eded: ')) # 4
+
+#     return num1 + num2
+
+
+# print(multiply())
+
+
+# 2
+def multiply(num1=0, num2=0):
+    return num1 + num2
+
+
+a = int(input('1-ci eded: '))
+b = int(input('2-ci eded: '))
+
+
+print(multiply(a, b))
+
+
+# 3
+# def multiply(num1: int=0, num2: int=0) -> int:
+#     return num1 + num2
+
+
+# a = int(input('1-ci eded: '))
+# b = int(input('2-ci eded: '))
+
+
+# print(multiply())
---
+++ while.py
@@ -0,0 +1,57 @@
+# Loops
+# While loop - Şərtli dövr
+
+
+# while şərt:
+#     Gövdə
+
+# Sonsuz sekilde tekrarlanir. Bu formada istifade etmeyin!!!
+# while 5 < 6:
+#     print('salam')
+
+# while True:
+#     print('salam')
+
+
+# 1 - 10
+# counter = 1
+# while counter <= 10:
+#     print('{} nömrəli dövr'.format(counter))
+#     print(f'{counter} nömrəli dövr')
+#     counter += 1 # counter = counter + 1
+
+
+# 2 - 11
+# counter = 1
+# while counter <= 10:
+#     counter += 1
+#     print('{} nömrəli dövr'.format(counter))
+#     print(f'{counter} nömrəli dövr')
+
+
+# pswd = 'ruhi123'
+# while True:
+#     entered_pswd = input('Şifrənizi daxil edin: ')
+#     if entered_pswd == pswd:
+#         print('Şifrəni doğru daxil etdiniz!')
+#         break
+#     else:
+#         print('Daxil etdiyiniz şifrə doğru deyil!')
+#         continue
+
+
+# pswd = 'ruhi123'
+# while True:
+#     entered_pswd = input('Şifrənizi daxil edin: ')
+#     if entered_pswd == pswd:
+#         print('Şifrəni doğru daxil etdiniz!')
+#         break
+#     print('Daxil etdiyiniz şifrə doğru deyil!')
+
+
+# while True:
+#     a = int(input('eded daxil et: '))
+#     if a % 2 == 0:
+#         print('Daxil etdiyiniz eded cutdur!')
+#     else:
+#         print('Daxil etdiyiniz eded tekdir!')
